1c6a53063bc48ec131b0036182875c0f730ca262
Andrew Givens <givenand@amazon.com>
2023-11-23 10:10:38 -0500

    updated dashboard.jsons

--- src/twinfleetcdk/lib/twinfleet-stack.ts
+++ src/twinfleetcdk/lib/twinfleet-stack.ts
@@ -144,7 +144,7 @@ export class TwinfleetStack extends Stack {
     //          This is a deployment time issue.
     //          This is expected to be resolved in a couple of months.  Have not seen this issue with
     //          python, only with typescript.
-    const VEHICLES_IN_FLEET: number = 20;
+    const VEHICLES_IN_FLEET: number = 21;
     const VEHICLE_BASE_NUMBER = 100;
 
     // Create the Fleet View scene
